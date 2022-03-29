6d7081bc6d9eb912b8ced0e09fe48b2fc19e7b1b
Tjeerd Smid <t.a.smid@students.uu.nl>
2022-03-29 09:58:07 +0200

    working on animations

--- frontend/src/Frontend.hs
+++ frontend/src/Frontend.hs
@@ -3,26 +3,48 @@
 {-# LANGUAGE PartialTypeSignatures #-}
 {-# LANGUAGE RecursiveDo #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE QuasiQuotes #-}
 
 module Frontend where
 
-import Common.Api
 import Common.Route
-import Control.Monad
-import Control.Monad.IO.Class
 import Control.Monad.Fix
 import Control.Monad.Trans (liftIO)
 import qualified Data.Text as T
-import qualified Data.Text.Encoding as T
 import qualified Data.Map as Map
 import qualified Data.Time as Time
-import Language.Javascript.JSaddle (eval, liftJSM)
-import Obelisk.Configs
 import Obelisk.Frontend
 import Obelisk.Generated.Static
 import Obelisk.Route
 import Reflex.Dom.Core
-import Language.Javascript.JSaddle
+
+ 
+data Player = One 
+type Seconds = Int
+type Point = (Int, Int)
+
+data Arrow = Arrow
+  {
+    start :: Point,
+    end :: Point
+  }
+
+data Movement = Movement
+  {
+    player :: Player,
+    arrow :: Arrow,
+    startTime :: Seconds,
+    endTime :: Seconds
+  }
+
+renderFrames :: Movement -> [Point]
+renderFrames mov  = 
+  let 
+    travelTime = min 0 $ (startTime mov) - (endTime mov)
+
+    diff (x1, y1) (x2,y2) = (x2 - x1, y2 - y1) 
+  in
+    undefined
 
 -- This runs in a monad that can be run on the client or the server.
 -- To run code in a pure client or pure server context, use one of the
@@ -39,11 +61,15 @@ frontend =
         el "div" $ do
           dETick <- prerender (return never) $ do
             new <- liftIO Time.getCurrentTime
-            eTick <- tickLossy 3 new
-            return $ T.pack . show . _tickInfo_n <$> eTick
+            eTick <- tickLossy 2 new
+            return $ T.pack . show . _tickInfo_n <$> eTick -- _tickInfo_n <$> -> gets amount of ticks as integer
           dynText =<< (holdDyn "No Ticks Yet" $ switchDyn $ dETick)
-          
-        return () --}
+        
+        rec el "div" $ text "Counter as fold"
+            numbs <- foldDyn (+) (0 :: Int)  (1 <$ evIncr)
+            el "div" $ display numbs
+            evIncr <- button "Increment"
+        return ()
     }
 
 toText :: (Show a) => a -> T.Text
@@ -52,12 +78,27 @@ toText =  T.pack . show
 svg :: (DomBuilder t m) => Int -> Int -> m a2 -> m a2
 svg h w = elAttr "svg" ("width" =: (toText h) <> "height" =: (toText w)) 
  
-
 circle :: (DomBuilder t m) => Int -> Int -> Int -> m a -> m a
 circle x y r = elAttr "circle" (circleAttr x y r)
 
 circleAttr :: Int -> Int -> Int -> Map.Map T.Text T.Text
 circleAttr x y r = ("cx" =: (toText x) <> "cy" =: (toText y)<> "r" =: (toText r) <> "fill" =: "yellow")
+
+followMovement :: (DomBuilder t m, MonadHold t m, PostBuild t m, MonadFix m) => m ()
+followMovement = do
+  rec 
+    
+    evIncr <- button "Next step"
+  return ()
+
+getFrameAt :: Movement -> Int -> Map.Map T.Text T.Text
+getFrameAt mov pos =
+    let
+        allFrames = renderFrames
+    in if length allFrames > pos 
+        then Map.singleton k a init allFrames 
+        else allFrames !! pos_
+
 
 clickMoveCircle :: (DomBuilder t m, MonadHold t m, PostBuild t m, MonadFix m) => m ()
 clickMoveCircle = do 
@@ -72,14 +113,3 @@ clickMoveCircle = do
 attrs :: Bool -> Map.Map T.Text T.Text
 attrs True = circleAttr 100 100 100
 attrs False = circleAttr 200 100 100
-
-bodyElement :: (DomBuilder t m,
-                  Control.Monad.IO.Class.MonadIO (Performable m),
-                  Control.Monad.IO.Class.MonadIO m, TriggerEvent t m,
-                  PerformEvent t m, PostBuild t m, MonadHold t m, MonadFix m) =>  m ()
-bodyElement = do
-  el "h2" $ text "A Simple Clock"
-  now <- liftIO Time.getCurrentTime
-  evTick <- tickLossy 1 now
-  let evTime = (T.pack . show . _tickInfo_lastUTC) <$>  evTick
-  dynText =<< holdDyn "No ticks yet" evTime
