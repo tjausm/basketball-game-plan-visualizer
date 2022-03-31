45188f682fe9a7ffa45aaebb807906516add7abd
Tjeerd Smid <t.a.smid@students.uu.nl>
2022-03-31 16:24:09 +0200

    Added basketball court and realistic arrows. Everything working now

--- frontend/src/Frontend.hs
+++ frontend/src/Frontend.hs
@@ -28,7 +28,10 @@ frontend =
   Frontend
     { _frontend_head = do
         el "title" $ text "Obelisk Minimal Example"
-        elAttr "link" ("href" =: "main.css" <> "type" =: "text/css" <> "rel" =: "stylesheet") blank,
+
+        -- import Bootstrap and fontawesome from cdn
+        elAttr "link" ("href" =: "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" <> "rel" =: "stylesheet") blank
+        elAttr "link" ("href" =: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" <> "rel" =: "stylesheet") blank,
       _frontend_body = do
         prerender_ blank (renderView [mov1, mov2, mov3, mov5])
 
@@ -38,20 +41,34 @@ frontend =
 -- Renders the complete basketball court, players and controls
 renderView :: (MonadWidget t m) => [Movement] -> m ()
 renderView movements = do
-
-  let fps = 30
-
-  rec t0 <- liftIO Time.getCurrentTime
+  let fps = 30 -- hardcode frames per second to 30
+  rec -- initialise timer
+      t0 <- liftIO Time.getCurrentTime
       eTick <- tickLossy (1 / toNDT fps) t0
+
+      -- get the arrows and render the movements
       let arrows = map arrow movements
       renderedMovements <- mapM (renderMovements fps eStart ePause eReset eTick) movements
-      svgBBalCourt $ do
-        mapM_ drawArrow arrows
-        (mapM (\m -> elDynSvgAttr "circle" m blank) renderedMovements :: _)
-      eStart <- button "Start"
-      ePause <- button "Pause"
-      eReset <- button "Reset"
-  return ()
+
+      -- draw the BBalCourt, arrows and the player animations
+      rowCenter $ do
+        colCenter 12 $ do
+          drawSvgBody 1400 1000 $ do
+            mapM_ drawArrow arrows
+            mapM_ (\m -> elDynSvgAttr "circle" m blank) renderedMovements
+          elAttr "img" ("src" =: "/static/1shk1nm322652sism9gqw8sbz34dangql5fs9f51ibnclqypjbb8-bbcourt.jpg") blank
+
+
+      -- draw the start, pause and reset button
+      (eStart, ePause, eReset) <-
+        rowCenter $ do
+          colCenter 12 $ do
+            s <- playButton
+            p <- pauseButton
+            r <- resetButton
+            return (s, p, r)
+
+  blank
 
 -- creates 1 dynamic map linked to the ticks and start & reset button
 renderMovements :: (DomBuilder t m, MonadWidget t m) => Int -> Event t () -> Event t () -> Event t () -> Event t TickInfo -> Movement -> m (Dynamic t (Map.Map T.Text T.Text))
@@ -63,10 +80,36 @@ renderMovements fps eStart ePause eReset eTick mov = do
 toNDT :: (Real a) => a -> Time.NominalDiffTime
 toNDT = fromRational . toRational
 
--- Draw calculations
-type Height = Int
+----------------
+-- UI & bootstrap elements--
+----------------
+buttonWithIcon :: DomBuilder t m => T.Text -> T.Text -> m (Event t ())
+buttonWithIcon c icon = do
+  (e, _) <- elAttr' "button" ("type" =: "button" <> "class" =: c) (elClass "i" icon blank)
+  return $ domEvent Click e
 
+playButton :: DomBuilder t m => m (Event t ())
+playButton = buttonWithIcon "btn btn-lg btn-primary me-1" "fa-solid fa-play"
+
+pauseButton :: DomBuilder t m => m (Event t ())
+pauseButton = buttonWithIcon "btn btn-lg btn-secondary me-1" "fa-solid fa-pause"
+
+resetButton :: DomBuilder t m => m (Event t ())
+resetButton = buttonWithIcon "btn btn-lg btn-danger" "fa-solid fa-stop"
+
+rowCenter :: DomBuilder t m => m a -> m a
+rowCenter = elClass "div" "row justify-content-center"
+
+colCenter :: DomBuilder t m => Int -> m a -> m a
+colCenter size = elAttr "div" $ "class" =: T.pack ("col-" ++ show size) <> "align" =: "center"
+
+-----------------
+-- SVG elements--
+-----------------
+
+type Height = Int
 type Width = Int
+type Color = T.Text
 
 -- using elDynAttrNS' to build svg in prerender (https://github.com/obsidiansystems/obelisk/issues/828)
 elDynSvgAttr ::
@@ -77,22 +120,15 @@ elDynSvgAttr ::
   m (Element EventResult (DomBuilderSpace m) t, a)
 elDynSvgAttr = elDynAttrNS' (Just "http://www.w3.org/2000/svg")
 
-svgBody :: (DomBuilder t m, PostBuild t m) => Height -> Width -> m a -> m (Element EventResult (DomBuilderSpace m) t, a)
-svgBody h w = elDynSvgAttr "svg" $ constDyn $ "height" =: toText h <> "width" =: toText w
+-- added position absolute to draw svg body over images
+drawSvgBody :: (DomBuilder t m, PostBuild t m) => Height -> Width -> m a -> m (Element EventResult (DomBuilderSpace m) t, a)
+drawSvgBody h w = elDynSvgAttr "svg" $ constDyn $ "height" =: toText h <> "width" =: toText w <> "style" =: "position:absolute"
 
-svgRect :: (DomBuilder t m, PostBuild t m) => Point -> Height -> Width -> T.Text -> m (Element EventResult (DomBuilderSpace m) t, ())
-svgRect (x, y) h w style = elDynSvgAttr "rect" (constDyn $ "x" =: toText x <> "y" =: toText y <> "height" =: toText h <> "width" =: toText w <> "style" =: style) blank
+drawRect :: (DomBuilder t m, PostBuild t m) => Point -> Height -> Width -> T.Text -> m (Element EventResult (DomBuilderSpace m) t, ())
+drawRect (x, y) h w style = elDynSvgAttr "rect" (constDyn $ "x" =: toText x <> "y" =: toText y <> "height" =: toText h <> "width" =: toText w <> "style" =: style) blank
 
-svgBBalCourt :: (MonadWidget t m) => m a -> m ()
-svgBBalCourt m = do
-  svgBody 800 1000 $ do
-    svgRect (0, 0) 800 1000 "fill:white;stroke:black;stroke-width:5;opacity:1"
-    svgRect (250, 0) 400 500 "fill:brown;stroke:black;stroke-width:5;opacity:0.5"
-    m
-  return ()
-
-drawLine :: (DomBuilder t m, PostBuild t m) => Point -> Point -> Map.Map T.Text T.Text -> m (Element EventResult (DomBuilderSpace m) t, ())
-drawLine (x1, y1) (x2, y2) otherAttrs = elDynSvgAttr "line" (constDyn $ "x1" =: toText x1 <> "y1" =: toText y1 <> "x2" =: toText x2 <> "y2" =: toText y2 <> "stroke-width" =: "5" <> "style" =: "fill:black;stroke:black" <> otherAttrs) blank
+drawLine :: (DomBuilder t m, PostBuild t m) => Width -> Point -> Point -> Map.Map T.Text T.Text -> m (Element EventResult (DomBuilderSpace m) t, ())
+drawLine strokeWidth (x1, y1) (x2, y2) otherAttrs = elDynSvgAttr "line" (constDyn $ "x1" =: toText x1 <> "y1" =: toText y1 <> "x2" =: toText x2 <> "y2" =: toText y2 <> "stroke-width" =: toText strokeWidth <> "style" =: "fill:black;stroke:black" <> otherAttrs) blank
 
 drawArrow :: (DomBuilder t m, PostBuild t m) => Arrow -> m (Element EventResult (DomBuilderSpace m) t, ())
 drawArrow a = do
@@ -100,13 +136,16 @@ drawArrow a = do
   elDynSvgAttr "marker" (constDyn $ "id" =: "triangle" <> "viewBox" =: "0 0 10 10" <> "markerUnits" =: "strokeWidth" <> "markerWidth" =: "4" <> "markerHeight" =: "3" <> "refX" =: "0" <> "refY" =: "5" <> "orient" =: "auto") $ do
     elDynSvgAttr "path" (constDyn $ "d" =: "M 0 0 L 10 5 L 0 10 z") blank
   elAttr "polygon" ("points" =: "0 0, 10 3.5, 0 7") blank
-  drawLine p1 p2 ("marker-end" =: "url(#triangle)")
+  drawLine 7 p1 p2 ("marker-end" =: "url(#triangle)")
 
 toText :: (Show a) => a -> T.Text
 toText = T.pack . show
 
-circleAttr :: Int -> Point -> Map.Map T.Text T.Text
-circleAttr r (x, y) = "cx" =: toText x <> "cy" =: toText y <> "r" =: toText r <> "fill" =: "black"
+playerAttr :: Point -> Map.Map T.Text T.Text
+playerAttr p = circleAttr "BurlyWood" 30 p
+
+circleAttr :: Color -> Int -> Point -> Map.Map T.Text T.Text
+circleAttr color r (x, y) = "cx" =: toText x <> "cy" =: toText y <> "r" =: toText r <> "fill" =: color
 
 -------------------------
 -- Movement calculations-
@@ -121,11 +160,12 @@ newtype Player = Player T.Text
 data Arrow = One | Two | Three | Four
   deriving (Show)
 
+-- hard code the coordinates of begin and end point of the arrows here
 getPoints :: Arrow -> (Point, Point)
-getPoints One = ((100, 100), (500, 500))
-getPoints Two = ((100, 100), (100, 500))
-getPoints Three = ((500, 100), (100, 500))
-getPoints Four = ((300, 500), (300, 100))
+getPoints One = ((800, 1100), (800, 300))
+getPoints Two = ((200, 300), (400, 200))
+getPoints Three = ((200, 500), (500, 300))
+getPoints Four = ((400, 1200), (500, 700))
 
 start :: Arrow -> Point
 start = fst . getPoints
@@ -183,4 +223,4 @@ getFrameAt :: Movement -> Int -> Int -> Map.Map T.Text T.Text
 getFrameAt mov framesPerSecond frameN =
   let allFrames = renderFrames mov framesPerSecond
       currFrame = if length allFrames > frameN then allFrames !! frameN else last allFrames
-   in circleAttr 10 currFrame
+   in playerAttr currFrame
