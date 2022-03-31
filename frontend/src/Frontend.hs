fc69440d7558e73ac906742072f85ecc640a57b4
Tjeerd Smid <t.a.smid@students.uu.nl>
2022-03-31 15:00:58 +0200

    Lines are now drawn to indicate player movement

--- frontend/src/Frontend.hs
+++ frontend/src/Frontend.hs
@@ -1,10 +1,10 @@
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE MonoLocalBinds #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE PartialTypeSignatures #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE RecursiveDo #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE MonoLocalBinds #-}
 {-# OPTIONS_GHC -Wno-unused-do-bind #-}
 
 module Frontend where
@@ -16,6 +16,7 @@ import qualified Data.Text as T
 import qualified Data.Time as Time
 import GHC.Float (float2Int, int2Float)
 import Obelisk.Frontend
+import Obelisk.Generated.Static (static)
 import Obelisk.Route
 import Reflex.Dom.Core
 
@@ -29,32 +30,36 @@ frontend =
         el "title" $ text "Obelisk Minimal Example"
         elAttr "link" ("href" =: "main.css" <> "type" =: "text/css" <> "rel" =: "stylesheet") blank,
       _frontend_body = do
-        prerender_ blank $ renderView
+        prerender_ blank renderView
+
         blank
     }
 
 -- Renders the complete basketball court, players and controls
-renderView :: (MonadWidget t m) =>  m ()
+renderView :: (MonadWidget t m) => m ()
 renderView = do
-        let speedFactor = 2 -- todo make this configurable
-        -- here we keep the fps constant at 30 regardless of the 'speedFactor'
-        -- e.g. 2 seconds playtime at 0.5 speed and 30 fps is  2/0.5*30=120 fps
-        let fps = float2Int (1 / speedFactor * 30)
-        let speed = 1 / (toNDT speedFactor * toNDT fps)
-        rec
-          t0 <- liftIO Time.getCurrentTime
-          eTick <- tickLossy speed t0
-          movements <- mapM (renderMovements fps eStart ePause eTick) [mov1,mov2,mov3,mov4,mov5]
-          svgBody 1000 750 $ do
-            mapM (\m -> elDynSvgAttr "circle" m blank) movements
-          eStart <- button "Start"
-          ePause <- button "Reset"
-        return ()
+  let movList = [mov1, mov2, mov3, mov5] -- TODO make configurable
+  -- here we keep the fps constant at 30 regardless of the 'speedFactor'
+  -- e.g. 2 seconds playtime at 0.5 speed and 30 fps is  2/0.5*30=120 fps
+  let speedFactor = 2 -- todo make this configurable
+  let fps = float2Int (1 / speedFactor * 30)
+  let speed = 1 / (toNDT speedFactor * toNDT fps)
+  rec t0 <- liftIO Time.getCurrentTime
+      eTick <- tickLossy speed t0
+      let arrows = map arrow movList
+      movements <- mapM (renderMovements fps eStart ePause eReset eTick) movList
+      svgBBalCourt $ do
+        mapM_ drawArrow arrows
+        mapM (\m -> elDynSvgAttr "circle" m blank) movements
+      eStart <- button "Start"
+      ePause <- button "Pause"
+      eReset <- button "Reset"
+  return ()
 
 -- creates 1 dynamic map linked to the ticks and start & reset button
-renderMovements :: (DomBuilder t m, MonadWidget t m) => Int -> Event t () -> Event t () -> Event t TickInfo -> Movement -> m (Dynamic t (Map.Map T.Text T.Text))
-renderMovements fps eStart ePause eTick mov = do
-  beStartStop <- hold never . leftmost $ [ (const 0 <$ eTick) <$ ePause, ((1+) <$ eTick) <$ eStart ]
+renderMovements :: (DomBuilder t m, MonadWidget t m) => Int -> Event t () -> Event t () -> Event t () -> Event t TickInfo -> Movement -> m (Dynamic t (Map.Map T.Text T.Text))
+renderMovements fps eStart ePause eReset eTick mov = do
+  beStartStop <- hold never . leftmost $ [((1 +) <$ eTick) <$ eStart, ((0 +) <$ eTick) <$ ePause, (const 0 <$ eTick) <$ eReset]
   let eSwitch = switch beStartStop
   fmap (getFrameAt mov fps) <$> foldDyn ($) 0 eSwitch
 
@@ -64,15 +69,43 @@ toNDT = fromRational . toRational
 -- Draw calculations
 
 -- using elDynAttrNS' to build svg in prerender (https://github.com/obsidiansystems/obelisk/issues/828)
-elDynSvgAttr  :: (DomBuilder t m, PostBuild t m)
-  => T.Text
-  -> Dynamic t (Map.Map T.Text T.Text)
-  -> m a
-  -> m (Element EventResult (DomBuilderSpace m) t, a)
+
+type Height = Int
+
+type Width = Int
+
+elDynSvgAttr ::
+  (DomBuilder t m, PostBuild t m) =>
+  T.Text ->
+  Dynamic t (Map.Map T.Text T.Text) ->
+  m a ->
+  m (Element EventResult (DomBuilderSpace m) t, a)
 elDynSvgAttr = elDynAttrNS' (Just "http://www.w3.org/2000/svg")
 
-svgBody :: (DomBuilder t m, PostBuild t m) => Int -> Int -> m a3 -> m (Element EventResult (DomBuilderSpace m) t, a3)
-svgBody h w = elDynSvgAttr "svg" $ constDyn $ "width" =: toText h <> "height" =: toText w
+svgBody :: (DomBuilder t m, PostBuild t m) => Height -> Width -> m a -> m (Element EventResult (DomBuilderSpace m) t, a)
+svgBody h w = elDynSvgAttr "svg" $ constDyn $ "height" =: toText h <> "width" =: toText w
+
+svgRect :: (DomBuilder t m, PostBuild t m) => Point -> Height -> Width -> T.Text -> m (Element EventResult (DomBuilderSpace m) t, ())
+svgRect (x, y) h w style = elDynSvgAttr "rect" (constDyn $ "x" =: toText x <> "y" =: toText y <> "height" =: toText h <> "width" =: toText w <> "style" =: style) blank
+
+svgBBalCourt :: (MonadWidget t m) => m a -> m ()
+svgBBalCourt m = do
+  svgBody 800 1000 $ do
+    svgRect (0, 0) 800 1000 "fill:white;stroke:black;stroke-width:5;opacity:1"
+    svgRect (250, 0) 400 500 "fill:brown;stroke:black;stroke-width:5;opacity:0.5"
+    m
+  return ()
+
+drawLine :: (DomBuilder t m, PostBuild t m) => Point -> Point -> Map.Map T.Text T.Text -> m (Element EventResult (DomBuilderSpace m) t, ())
+drawLine (x1, y1) (x2, y2) otherAttrs = elDynSvgAttr "line" (constDyn $ "x1" =: toText x1 <> "y1" =: toText y1 <> "x2" =: toText x2 <> "y2" =: toText y2 <> "stroke-width" =: "5" <> "style" =: "fill:black;stroke:black" <> otherAttrs) blank
+
+drawArrow :: (DomBuilder t m, PostBuild t m) => Arrow -> m (Element EventResult (DomBuilderSpace m) t, ())
+drawArrow a = do
+  let (p1, p2) = getPoints a
+  elDynSvgAttr "marker" (constDyn $ "id" =: "triangle" <> "viewBox" =: "0 0 10 10" <> "markerUnits" =: "strokeWidth" <> "markerWidth" =: "4" <> "markerHeight" =: "3" <> "refX" =: "0" <> "refY" =: "5" <> "orient" =: "auto") $ do
+    elDynSvgAttr "path" (constDyn $ "d" =: "M 0 0 L 10 5 L 0 10 z") blank
+  elAttr "polygon" ("points" =: "0 0, 10 3.5, 0 7") blank
+  drawLine p1 p2 ("marker-end" =: "url(#triangle)")
 
 toText :: (Show a) => a -> T.Text
 toText = T.pack . show
@@ -83,19 +116,27 @@ circleAttr r (x, y) = "cx" =: toText x <> "cy" =: toText y <> "r" =: toText r <>
 -------------------------
 -- Movement calculations-
 -------------------------
-
-data Player = One
-  deriving (Show)
-
 type Seconds = Int
 
 type Point = (Float, Float)
 
-data Arrow = Arrow
-  { start :: Point,
-    end :: Point
-  }
+newtype Player = Player T.Text
   deriving (Show)
+
+data Arrow = One | Two | Three | Four
+  deriving (Show)
+
+getPoints :: Arrow -> (Point, Point)
+getPoints One = ((100, 100), (500, 500))
+getPoints Two = ((100, 100), (100, 500))
+getPoints Three = ((500, 100), (100, 500))
+getPoints Four = ((300, 500), (300, 100))
+
+start :: Arrow -> Point
+start = fst . getPoints
+
+end :: Arrow -> Point
+end = snd . getPoints
 
 data Movement = Movement
   { player :: Player,
@@ -105,27 +146,20 @@ data Movement = Movement
   }
   deriving (Show)
 
-
-mkArrow :: Point -> Point -> Arrow
-mkArrow p1 p2 = Arrow {start = p1, end = p2}
-
 mkMovement :: Arrow -> Seconds -> Seconds -> Movement
-mkMovement a beginT endT = Movement {player = One, arrow = a, startTime = if beginT > endT then endT else beginT, endTime = endT}
+mkMovement a beginT endT = Movement {player = Player "One", arrow = a, startTime = if beginT > endT then endT else beginT, endTime = endT}
 
 mov1 :: Movement
-mov1 = mkMovement (mkArrow (100, 100) (500, 500)) 1 6
+mov1 = mkMovement One  1 6
 
 mov2 :: Movement
-mov2 = mkMovement (mkArrow (100, 100) (100, 500)) 0 10
+mov2 = mkMovement Two  0 10
 
 mov3 :: Movement
-mov3 = mkMovement (mkArrow (500, 100) (100, 500)) 0 3
-
-mov4 :: Movement
-mov4 = mkMovement (mkArrow (300, 500) (300, 100)) 0 5
+mov3 = mkMovement Three 0 3
 
 mov5 :: Movement
-mov5 = mkMovement (mkArrow (600, 100) (100, 500)) 0 5
+mov5 = mkMovement Four 0 5
 
 -- Difference between 2 points
 diff :: Point -> Point -> Point
@@ -138,9 +172,6 @@ divPoint (x, y) n = let n' = int2Float n in (x / n', y / n')
 -- return stepsize given the n of frames a movement takes
 stepSize :: Movement -> Int -> Point
 stepSize mov travTime = diff (start (arrow mov)) (end (arrow mov)) `divPoint` travTime
-
---framesPerSecond :: Int
---framesPerSecond = 60
 
 -- renders a list of 'frames' (= position on x and y axes of circle at moment in time)
 -- from it's starting frame to it's end frame with 30 frames per second
